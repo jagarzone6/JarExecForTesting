c7988d33eea856582afdb4f7eb99861e75836fac
jgarzon <jgarzon@easysol.net>
2016-04-18 20:10:26 -0500

    Automatized testing using GWT

--- src/test/java/com/Pages/InitAmazon.java
+++ src/test/java/com/Pages/InitAmazon.java
@@ -3,33 +3,29 @@ package com.Pages;
 /**
  * Created by jagarzone on 17/12/15.
  */
-import com.vaadin.testbench.TestBenchTestCase;
-import com.vaadin.testbench.elements.ButtonElement;
-import com.vaadin.testbench.elements.ImageElement;
-import com.vaadin.testbench.elements.PasswordFieldElement;
-import com.vaadin.testbench.elements.TextFieldElement;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
-public class InitAmazon extends TestBenchTestCase {
+public class InitAmazon {
     static String ID_TXT_INPUT = "twotabsearchtextbox";
     static String ID_SEARCH_BUTTON = "nav-search-submit-text";
+    WebDriver webDriver_;
 
-    TextFieldElement txtInput;
-    ButtonElement searchButton;
-    PasswordFieldElement passwordInput;
-    ImageElement imageBanner;
-    public InitAmazon(WebDriver webDriver){setDriver(webDriver);}
+    public InitAmazon(WebDriver webDriver){webDriver_=webDriver;}
 
     public void searchText(String txt){
         //txtInput =  $(TextFieldElement.class).id(ID_TXT_INPUT);
         //txtInput.sendKeys(txt);
-        driver.findElement(By.id(ID_TXT_INPUT)).clear();
-        driver.findElement(By.id(ID_TXT_INPUT)).sendKeys(txt);
+       WebElement textinput = webDriver_.findElement(By.id(ID_TXT_INPUT));
+        textinput.clear();
+        textinput.sendKeys(txt);
     }
     public void clickBuscar() throws InterruptedException {
         //searchButton = $(ButtonElement.class).id(ID_SEARCH_BUTTON);
-        driver.findElement(By.id(ID_SEARCH_BUTTON)).click();
+        WebElement searchbutton = webDriver_.findElement(By.id(ID_SEARCH_BUTTON));
+        searchbutton.click();
         //driver.findElement(By.cssSelector("input.nav-input")).click();
         Thread.sleep(5000);
     }
--- src/test/java/com/Pages/InitGoogle.java
+++ src/test/java/com/Pages/InitGoogle.java
@@ -5,33 +5,29 @@ package com.Pages;
  */
 
 
-import com.vaadin.testbench.TestBenchTestCase;
-import com.vaadin.testbench.elements.ButtonElement;
-import com.vaadin.testbench.elements.ImageElement;
-import com.vaadin.testbench.elements.PasswordFieldElement;
-import com.vaadin.testbench.elements.TextFieldElement;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
-public class InitGoogle extends TestBenchTestCase {
+public class InitGoogle  {
     static String ID_TXT_INPUT = "lst-ib";
     static String ID_BUSCAR_BUTTON = "btnG";
 
-    TextFieldElement txtInput;
-    ButtonElement searchButton;
-    PasswordFieldElement passwordInput;
-    ImageElement imageBanner;
 
-    public InitGoogle(WebDriver webDriver){setDriver(webDriver);}
+    WebDriver webDriver_;
+
+    public InitGoogle(WebDriver webDriver){webDriver_=webDriver;}
 
     public void searchText(String txt){
     //txtInput =  $(TextFieldElement.class).id(ID_TXT_INPUT);
     //txtInput.sendKeys(txt);
-        driver.findElement(By.id(ID_TXT_INPUT)).clear();
-        driver.findElement(By.id(ID_TXT_INPUT)).sendKeys(txt);
+       WebElement txtinput = webDriver_.findElement(By.id(ID_TXT_INPUT));
+        txtinput.clear();
+        txtinput.sendKeys(txt);
     }
     public void clickBuscar() throws InterruptedException {
-        driver.findElement(By.name(ID_BUSCAR_BUTTON)).click();
+        WebElement button = webDriver_.findElement(By.name(ID_BUSCAR_BUTTON));
+        button.click();
         Thread.sleep(5000);
     }
 }
