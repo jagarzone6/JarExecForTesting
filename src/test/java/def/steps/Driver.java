18588fc1a6c6859acd383dae5f31386ed9031e91
jorge <jagarzone@gmail.com>
2016-04-29 22:14:36 -0500

    Windows exec for chrome and firefox and IE

--- src/test/java/def/steps/Driver.java
+++ src/test/java/def/steps/Driver.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.firefox.internal.ProfilesIni;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
@@ -21,34 +22,30 @@ public class Driver {
 
     static WebDriver driver;
     static DesiredCapabilities caps;
-
-
-
-
-
-    static StringBuffer verificationErrors = new StringBuffer();
+    static String IEdriverPath="C:\\Users\\jorge\\Downloads\\";
+    static String ChromedriverPath="C:\\Users\\jorge\\Downloads\\";
 
     public static WebDriver getNewDriver() throws MalformedURLException {
-
+        DesiredCapabilities capabilities = DesiredCapabilities.internetExplorer();
+        capabilities.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS,true);
         // driver = new RemoteWebDriver(new URL("http://jgarzon%40easysol.net:ub8f17b60626af2b@hub.crossbrowsertesting.com:80/wd/hub"), Driver.getCaps());
         //System.out.println("Loading Url");
-        //System.setProperty("webdriver.chrome.driver", "C:\\Users\\jorge\\Downloads\\chromedriver.exe");
+        //System.setProperty("webdriver.chrome.driver", ChromedriverPath+"chromedriver.exe");
         //driver = new ChromeDriver();
-        ProfilesIni profile = new ProfilesIni();
-        FirefoxProfile myprofile = profile.getProfile("C:\\Users\\jorge\\Appdata\\Roaming\\Mozilla\\Firefox\\Profiles\\8zk3r37j.beta");
-        driver = new FirefoxDriver();
-
+        //ProfilesIni profile = new ProfilesIni();
+        //FirefoxProfile myprofile = profile.getProfile("C:\\Users\\jorge\\Appdata\\Roaming\\Mozilla\\Firefox\\Profiles\\8zk3r37j.beta");
+        //driver = new FirefoxDriver();
+        System.setProperty("webdriver.ie.driver", IEdriverPath+"IEDriverServer.exe");
+        driver = new InternetExplorerDriver(capabilities);
+        driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        //   driver = new InternetExplorerDriver();
+
         return driver;
     }
 
     public static void deleteInstanceBrowser() throws Exception {
         driver.quit();
-        String verificationErrorString = verificationErrors.toString();
-        if (!"".equals(verificationErrorString)) {
-            fail(verificationErrorString);
-        }
+
 
     }
 }
